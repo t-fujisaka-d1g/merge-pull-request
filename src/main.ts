3f1ca97447b1f2b75e85f92be5b60f703d95e390
t-fujisaka <t-fujisaka@daiichi-g.co.jp>
2022-07-27 17:26:45 +0900

    プルリクエストにコメントを追加

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token: string = core.getInput('github-token')
+    const pullNumber: string = core.getInput('pull-number')
+    core.debug(`pullNumber: ${pullNumber}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = github.getOctokit(token)
 
-    core.setOutput('time', new Date().toTimeString())
+    // コメントを書き込む
+    const result = await octokit.rest.issues.createComment({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      issue_number: Number(pullNumber),
+      body: '書き込みテスト'
+    })
+
+    core.debug(`result: ${JSON.stringify(result, null, '  ')}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
