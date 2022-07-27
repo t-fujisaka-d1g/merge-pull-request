76d088f02ab84d95de2d7f5e2f8fc7f443118def
t-fujisaka <t-fujisaka@daiichi-g.co.jp>
2022-07-28 08:43:50 +0900

    プルリクエストの番号をgithub.contextから取得

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,25 @@ import * as github from '@actions/github'
 async function run(): Promise<void> {
   try {
     const token: string = core.getInput('github-token')
-    const pullNumber: string = core.getInput('pull-number')
-    core.debug(`pullNumber: ${pullNumber}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const pullRequest = github.context.payload.pull_request
+
+    if (!pullRequest) {
+      // プルリクエスト以外のイベントで実行された場合は終了
+      core.setFailed('E0001')
+      return
+    }
 
     core.debug('======================== context ========================')
     core.debug(`${JSON.stringify(github.context, null, '  ')}`)
     core.debug('=========================================================')
-    
+
     const octokit = github.getOctokit(token)
 
     // コメントを書き込む
     const result = await octokit.rest.issues.createComment({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      issue_number: Number(pullNumber),
+      issue_number: pullRequest.number,
       body: '書き込みテスト'
     })
 
